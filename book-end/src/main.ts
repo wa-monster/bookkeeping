23dc0a4d677856cd4acfb5556b35aa7ae7ff14cd
wa-monster <2037485086@qq.com>
2022-08-11 16:52:26 +0800

    基础练习

--- book-end/src/main.ts
+++ book-end/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { TransformInterceptor } from './common/interceptors/transform.interceptor'
+// 换成Fastify框架
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+  app.useGlobalInterceptors(new TransformInterceptor)
+  // 接口版本化管理
+  // 可以全局配置请求控制
+  app.enableVersioning({
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    type: VersioningType.URI
+  })
+
   await app.listen(3000);
 }
 bootstrap();
--- book-end/src/user/user.controller.ts
+++ book-end/src/user/user.controller.ts
@@ -1,22 +1,31 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Version, Get, Post, Body, Patch, Param, Delete, VERSION_NEUTRAL } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
-@Controller('user')
+// Version标识这个接口的都是1.0版本的
+@Controller({
+  path: 'user',
+  version: '1'
+})
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
+  @Version([VERSION_NEUTRAL, '1'])
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @Version([VERSION_NEUTRAL, '1'])
   findAll() {
     return this.userService.findAll();
   }
-
+  @Get()
+  @Version('2')
+  findAll2() {
+    return 'i am v2';
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
