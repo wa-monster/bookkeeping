67c702d659a1f46de428c09b1654dc5bf4ad46d0
yangjie <2037485086@qq.com>
2022-10-07 16:41:05 +0800

    feat: css改为scss

--- book-front/src/layout/components/MenuDown.tsx
+++ book-front/src/layout/components/MenuDown.tsx
@@ -10,13 +10,7 @@ const MenuDown = ()=> {
   return (
     <Menu onClick={handleMenuClick}>
       <Menu.Item key="1" icon={<UserOutlined />}>
-        1st menu item
-      </Menu.Item>
-      <Menu.Item key="2" icon={<UserOutlined />}>
-        2nd menu item
-      </Menu.Item>
-      <Menu.Item key="3" icon={<UserOutlined />}>
-        3rd menu item
+        退出登录
       </Menu.Item>
     </Menu>
   )
--- book-front/src/layout/components/YangHeader.tsx
+++ book-front/src/layout/components/YangHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {  Dropdown } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
-import './YangHeader.css'
+import './YangHeader.scss'
 import MenuDown from './MenuDown'
 
 const YangHeader =  () => {
@@ -9,7 +9,7 @@ const YangHeader =  () => {
   return (
     <div className="header-box">
       <div className="header-content">
-        <h1>记账</h1>
+        <h1>杨大爷记账</h1>
         <Dropdown.Button overlay={MenuDown} placement="bottomCenter" icon={<UserOutlined />}>
           Dropdown
         </Dropdown.Button>
