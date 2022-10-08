f28f923f9ec8dcc4696d2cf912cd4d849e4a64d3
yangjie <2037485086@qq.com>
2022-10-08 18:38:23 +0800

    tij

---
+++ book-front/main.js
@@ -0,0 +1,31 @@
+// main.js
+
+
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+function createWindow () {
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  // 加载 index.html
+  // mainWindow.loadFile('index.html')
+  mainWindow.loadURL('http://localhost:3000/');
+}
+
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+  })
+})
